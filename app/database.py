29c32a95176727bd61cb8e71e47d5af4ee383573
DTR322 <tasher-123456@mail.ru>
2024-12-13 19:25:58 +1000

    start

--- app/database.py
+++ app/database.py
@@ -10,7 +10,7 @@ DB_HOST = 'localhost'
 DB_PORT = '5432'
 DB_NAME = 'postgres'
 DB_USER = 'admin'
-DB_PASSWORD = '123'
+DB_PASSWORD = ''
 
 DATABASE_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
