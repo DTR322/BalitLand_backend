de82e2befd55bc0b2902fb8df67e90f2db9f81c5
DedCarlo <158022545+DedCarlo@users.noreply.github.com>
2024-12-16 19:43:42 +1000

    0.4

--- app/students/dao.py
+++ app/students/dao.py
@@ -1,12 +1,27 @@
-from sqlalchemy import select
-from app.students.models import Student
+from sqlalchemy import insert
+from sqlalchemy.exc import SQLAlchemyError
+
+from app.dao.base import BaseDAO
 from app.database import async_session_maker
+from app.students.models import Student
 
 
-class StudentDAO:
+class StudentDAO(BaseDAO):
+    model = Student
+
     @classmethod
-    async def find_all_students(cls):
+    async def add_student(cls, student_data: dict):
         async with async_session_maker() as session:
-            query = select(Student)
-            students = await session.execute(query)
-            return students.scalars().all()
+            async with session.begin():
+                # Вставка нового студента
+                stmt = insert(cls.model).values(**student_data).returning(cls.model.id,)
+                result = await session.execute(stmt)
+                new_student_id, major_id = result.fetchone()
+
+                try:
+                    await session.commit()
+                except SQLAlchemyError as e:
+                    await session.rollback()
+                    raise e
+
+                return new_student_id
--- app/students/router.py
+++ app/students/router.py
@@ -1,10 +1,19 @@
 from fastapi import APIRouter
 from app.students.dao import StudentDAO
-
+from app.students.schemas import SStudent, SStudentAdd
 
 router = APIRouter(prefix='/students', tags=['Работа с учениками'])
 
 
-@router.get("/", summary="Получить всех учеников")
+@router.get("/", summary="Получить всех учеников", response_model=list[SStudent])
 async def get_all_students():
-    return await StudentDAO.find_all_students()
+    return await StudentDAO.find_all()
+
+
+@router.post("/add/")
+async def register_user(student: SStudentAdd) -> dict:
+    check = await StudentDAO.add_student(**student.dict())
+    if check:
+        return {"message": "Ученик успешно добавлен!", "student": student}
+    else:
+        return {"message": "Ошибка при добавлении ученика!"}
