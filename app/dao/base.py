3afb590d64ba80e356158f9deedcd99045694c01
DTR322 <tasher-123456@mail.ru>
2024-12-16 20:18:42 +1000

    start

--- app/dao/base.py
+++ app/dao/base.py
@@ -40,17 +40,3 @@ class BaseDAO:
             query = select(cls.model).filter_by(**filter_by)
             result = await session.execute(query)
             return result.scalar_one_or_none()
-from sqlalchemy.future import select
-from app.database import async_session_maker
-
-
-class BaseDAO:
-    model = None
-
-    @classmethod
-    async def find_all(cls, **filter_by):
-        async with async_session_maker() as session:
-            query = select(cls.model).filter_by(**filter_by)
-            result = await session.execute(query)
-            return result.scalars().all()
-
